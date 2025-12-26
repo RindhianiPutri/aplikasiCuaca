aa7f414caf914bcc9b644ae38d8cbadc3639f6e2
Rindhiani Putri <rindi1826@gmail.com>
2025-12-26 11:57:38 +0700

    Jobsheet5

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -15,8 +15,8 @@ weatherform.addEventListener('submit', (e) => {
     pesanSatu.textContent = 'Sedang mencari lokasi ..'
     pesanDua.textContent = ''
 
-    fetch('http://localhost:4000/infocuaca?address=' + location).then((response) => {
-        response.json().then((data) => {
+    fetch('/infocuaca?address=' + location).then((response)=>{
+    response.json().then((data)=>{
             if (data.error) {
                 pesanSatu.textContent = data.error
             } else {
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,19 +6,23 @@ const forecast = require('./utils/prediksiCuaca')
 
 const app = express()
 
-// Mendefinisikan jalur/path untuk konfigurasi Express
-const direktoriPublic = path.join(__dirname, '../public')
-const direktoriViews = path.join(__dirname, '../templates/views')
-const direktoriPartials = path.join(__dirname, '../templates/partials')
+// Menentukan port dari environment (untuk deployment) atau default 3000 saat lokal
+const port = process.env.PORT || 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine dan lokasi folder views
 app.set('view engine', 'hbs')
-app.set('views', direktoriViews)
-hbs.registerPartials(direktoriPartials)
-// Setup direktori statis
-app.use(express.static(direktoriPublic))
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
 
-// ini halaman utama
+// Setup direktori statis
+app.use(express.static(publicDirectoryPath))
+
+// Halaman utama
 app.get('', (req, res) => {
     res.render('index', {
         judul: 'Aplikasi Cek Cuaca',
@@ -26,7 +30,7 @@ app.get('', (req, res) => {
     })
 })
 
-// ini halaman bantuan/FAQ (Frequently Asked Questions)
+// Halaman bantuan / FAQ
 app.get('/bantuan', (req, res) => {
     res.render('bantuan', {
         judul: 'Bantuan Saya',
@@ -35,21 +39,24 @@ app.get('/bantuan', (req, res) => {
     })
 })
 
-// ini halaman info cuaca
+// Halaman info cuaca
 app.get('/infocuaca', (req, res) => {
-    if(!req.query.address){
+    if (!req.query.address) {
         return res.send({
-            error:'Kamu harus memasukan lokasi yang ingin dicari'
+            error: 'Kamu harus memasukan lokasi yang ingin dicari'
         })
     }
-    geocode(req.query.address, (error, { latitude, longitude,location } = {}) => {
-        if (error){
-            return res.send({error})
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
         }
+
         forecast(latitude, longitude, (error, dataPrediksi) => {
-            if (error){
-                return res.send({error})
+            if (error) {
+                return res.send({ error })
             }
+
             res.send({
                 prediksiCuaca: dataPrediksi,
                 lokasi: location,
@@ -59,7 +66,7 @@ app.get('/infocuaca', (req, res) => {
     })
 })
 
-// ini halaman tentang
+// Halaman tentang
 app.get('/tentang', (req, res) => {
     res.render('tentang', {
         judul: 'Tentang Saya',
@@ -67,6 +74,7 @@ app.get('/tentang', (req, res) => {
     })
 })
 
+// Halaman 404 untuk bantuan
 app.get('/bantuan/*', (req, res) => {
     res.render('404', {
         judul: '404',
@@ -75,6 +83,7 @@ app.get('/bantuan/*', (req, res) => {
     })
 })
 
+// Halaman 404 umum
 app.get('*', (req, res) => {
     res.render('404', {
         judul: '404',
@@ -83,6 +92,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(4000, () => {
-    console.log('Server berjalan di port 4000')
+// Jalankan server
+app.listen(port, () => {
+    console.log('Server berjalan di port ' + port)
 })
